d8750f095eaf447b6fbac798e48cdeab59eb2ff3
Andres Gomez <andresxv98@gmail.com>
2025-03-14 11:24:01 -0500

    Add speedcontrol fn

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import { getObstacleEvents } from "./computer-vision";
 interface AutonomousCarProps {
     isRunning?: boolean;
     steeringControl: Steering;
+    speedControl: Speed;
 }
 
 interface Events {
@@ -23,14 +24,20 @@ interface Steering extends Control {
     turn: (direction: string) => void;
 }
 
+interface Speed extends Control {
+    pedal: (action: "accelerate" | "decelerate") => void;
+}
+
 //Classes Section
 class Car implements AutonomousCar {
     isRunning;
     steeringControl: Steering;
+    speedControl: Speed;
 
     constructor(props: AutonomousCarProps) {
         this.isRunning = props.isRunning;
         this.steeringControl = props.steeringControl;
+        this.speedControl = props.speedControl;
     }
 
     respond(events: Events) {
@@ -40,14 +47,21 @@ class Car implements AutonomousCar {
         }
         let eventKeys = Object.keys(events);
         eventKeys.forEach((eventKey) => {
-            if (!events[eventKey]) {
-                return;
-            }
-            if (eventKey === "ObstacleLeft") {
-                this.steeringControl.turn("right");
-            }
-            if (eventKey === "ObstacleRight") {
-                this.steeringControl.turn("left");
+            if (!events[eventKey]) return;
+
+            switch (eventKey) {
+                case "ObstacleLeft":
+                    this.steeringControl.turn("right");
+                    break;
+                case "ObstacleRight":
+                    this.steeringControl.turn("left");
+                    break;
+                case "SpeedUp":
+                    this.speedControl.pedal("accelerate");
+                    break;
+                case "SlowDown":
+                    this.speedControl.pedal("decelerate");
+                    break;
             }
         });
     }
@@ -63,12 +77,22 @@ class SteeringControl implements Steering {
     }
 }
 
+class SpeedControl implements Speed {
+    execute(command: string) {
+        console.log(`Executing: ${command}`);
+    }
+    pedal(action: "accelerate" | "decelerate") {
+        this.execute(`${action}`);
+    }
+}
+
 //Execution Section
 let steering = new SteeringControl();
-steering.turn("left");
+let speed = new SpeedControl();
 
-let autonomousCar = new Car({ isRunning: true, steeringControl: steering });
-autonomousCar.respond(getObstacleEvents());
-autonomousCar.respond(getObstacleEvents());
-autonomousCar.respond(getObstacleEvents());
+let autonomousCar = new Car({
+    isRunning: true,
+    steeringControl: steering,
+    speedControl: speed,
+});
 autonomousCar.respond(getObstacleEvents());
